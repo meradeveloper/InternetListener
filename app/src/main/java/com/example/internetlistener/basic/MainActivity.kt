3923c6153cbd6e4a384ae6836310b843de9a2668
sumit.rawat <sumit.rawat@sagoon.com>
2023-07-13 17:46:03 +0530

    create separate folder

--- app/src/main/java/com/example/internetlistener/basic/MainActivity.kt
+++ app/src/main/java/com/example/internetlistener/basic/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.internetlistener.basic
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
